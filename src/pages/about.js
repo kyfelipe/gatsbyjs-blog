9c4a474b1f1d9fd1cc66045e4fa528abd8342a84
Felipe Côrtes <contatos.cortes@gmail.com>
2020-02-29 22:27:23 -0300

    Adicionado componente de profile; Adicionado página de About; Adicionado primeira consulta no GraphQL.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const AboutPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Home</h1>
+    <SEO title="About" />
+    <h1>About Page</h1>
     <ul>
       <li>
         <a href={"/about"}>About</a>
@@ -20,6 +19,6 @@ const IndexPage = () => (
       </li>
     </ul>
   </Layout>
-)
+);
 
-export default IndexPage
+export default AboutPage;
