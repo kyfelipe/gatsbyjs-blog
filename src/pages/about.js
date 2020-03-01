287ba19e47b314aa97c7f92147515c7f3c9fa4db
Felipe Côrtes <contatos.cortes@gmail.com>
2020-03-01 11:16:08 -0300

    Create Sidebar Component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import Layout from "../components/Layout/layout"
+import Layout from "../components/Layout/index"
 import SEO from "../components/seo"
 
 const AboutPage = () => (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/Layout/layout"
+import Layout from "../components/Layout/index"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
