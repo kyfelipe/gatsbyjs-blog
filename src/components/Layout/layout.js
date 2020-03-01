85adc376b171a63b2cc9ad268cae1a145dfccf08
Felipe Côrtes <contatos.cortes@gmail.com>
2020-03-01 10:50:50 -0300

    Refactor styled components

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -1,42 +1,24 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import styled from "styled-components"
 
 import Profile from "../Profile"
+import * as S from './styled'
 import GlobalStyles from "../../styles/global"
-
-const LayoutWrapper = styled.section`
-  display: flex
-`;
-
-const LayoutMain = styled.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`;
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
       <aside>
         <Profile/>
       </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
