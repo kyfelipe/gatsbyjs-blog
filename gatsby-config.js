ee3e692378a0dfe2d91ba271996fe093acedffab
Felipe Côrtes <contatos.cortes@gmail.com>
2020-03-15 22:54:54 -0300

    Configure gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+
+const queries = require('./src/utils/algolia_queries');
+
 module.exports = {
   siteMetadata: {
     title: `My blog`,
@@ -8,7 +12,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
-    // needs to be the first to work with gatsbt-remark-images
+    // needs to be the first to work with gatsby-remark-images
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -54,6 +58,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000,
+        enablePartialUpdates: true,
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
